b1584b8da26199ce8446f8c0ca452a023abc18c9
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2025-01-21 14:25:47 +0100

    Reafactored tests

--- tests/test_env_parser.py
+++ tests/test_env_parser.py
@@ -19,11 +19,11 @@ from typedefs import MQTTParams
         ["example1.com:1234,example2.com:0", [("example1.com", 1234), ("example2.com", 1883)]],
     ],
 )
-def test_hostnames_pass(hosts, result):
+def test_mqtt_hostnames_pass(hosts, result):
     """
     Test parsing hostname(s) from env variables.
     """
-    env_params = MQTTParams(hosts=hosts, client_id="foo", username="bar", password="12345")
+    env_params = MQTTParams(hosts=hosts, identifier="foo", username="bar", password="12345")
 
     assert env_params.hosts == result
 
@@ -36,9 +36,9 @@ def test_hostnames_pass(hosts, result):
         ["example.com:-1", pytest.raises(ValidationError)],
     ],
 )
-def test_hostnames_fail(hosts, expectation):
+def test_mqtt_hostnames_fail(hosts, expectation):
     """
     Test parsing hostname(s) from env variables. Test invalid hostnames.
     """
     with expectation:
-        MQTTParams(hosts=hosts, client_id="foo", username="bar", password="12345")
+        MQTTParams(hosts=hosts, identifier="foo", username="bar", password="12345")
